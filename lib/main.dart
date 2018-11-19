741a2a1f8df2d6d9b604ebba6a49e2a7f8265af8
rjbx <rjbx@users.noreply.github.com>
2018-11-18 19:50:23 -0500

    feature: Add a UI for composing messages
    ChatScreen was modified to extend StatefulWidget and instantiate a State
    ChatScreenState was added to extend State and build a text composer for reading the contents of and clearing an input field
    ChatScreenState._buildTextComposer was added to wrap an IconTheme color around Rows setting adjacent Flexible TextFields and a send IconButton that clears input on submission

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(FriendlyChatApp());
+void main() => runApp(FlutterChatApp());
 
-class FriendlyChatApp extends StatelessWidget {
+class FlutterChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,13 +12,51 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("FlutterChat"),
       ),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onChanged: _handleSubmitted,
+                decoration: InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () =>_handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
